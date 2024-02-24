d7b1bb0244d7aeddb730521c91330088079687a8
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2024-02-24 11:15:29 +0100

    Add grid for Projects

--- app/_modules/projects/projects.tsx
+++ app/_modules/projects/projects.tsx
@@ -1,23 +1,26 @@
+import React from "react";
 import { ProjectCard } from "../../_components/project-card";
 import { Typography } from "@/app/_ui/typography";
 
 function Projects() {
   return (
-    <>
+    <div className="flex flex-col items-center">
       <Typography
         variant="h3"
-        className="text-center font-light uppercase tracking-wider mb-12"
+        className="mb-12 text-center font-light uppercase tracking-wider"
       >
         Projects
       </Typography>
-      <ProjectCard
-        title="Food Delivery Service"
-        description="Complete UI/UX design created on Figma. The project was created as a part of the university course."
-        previewImage="/project-placeholder.png"
-        previewUrl="#"
-        sourceUrl="#"
-      />
-    </>
+      <div className="grid max-w-fit grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+        <ProjectCard
+          title="Food Delivery Service"
+          description="Complete UI/UX design created on Figma. The project was created as a part of the university course."
+          previewImage="/project-placeholder.png"
+          previewUrl="#"
+          sourceUrl="#"
+        />
+      </div>
+    </div>
   );
 }
 
