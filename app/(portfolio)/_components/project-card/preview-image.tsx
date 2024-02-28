2027f86c667eb83c0ec2b54e8d5892db8553ac19
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2024-02-28 13:09:40 +0100

    Change quality

--- app/(portfolio)/_components/project-card/preview-image.tsx
+++ app/(portfolio)/_components/project-card/preview-image.tsx
@@ -77,7 +77,7 @@ export function PreviewImage(props: PreviewImageProps) {
       </motion.div>
       <Image
         src={props.src}
-        quality={90}
+        quality={100}
         width={300}
         height={300}
         alt={props.alt}
